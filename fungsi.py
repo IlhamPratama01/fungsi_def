4f5686bf62dff9d29d017f4f958fe77296fb6f9c
Ilham <muhamadilhampratama2@gmail.com>
2022-05-29 13:40:21 +0700

    def

---
+++ fungsi.py
@@ -0,0 +1,4 @@
+def selamat():
+    print("hai")
+
+selamat()   
---
+++ parameter.py
@@ -0,0 +1,13 @@
+def greetings(nama, waktu):
+    print("hai", nama, "selamat", waktu)
+    print('apakah', nama, 'sudah datang', waktu, 'ini')
+
+def jumlah(a,b):
+    hasil = a+b
+    return   hasil  
+
+greetings('ilham','siang')   
+greetings('rendi','pagi')   
+greetings('andi','malam')   
+
+greetings(waktu='malam',nama='akbar') 
